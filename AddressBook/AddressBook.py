b295834907066042bef60535dae7aeda0f6f0cf4
GavinDing <gavin_ding@sina.cn>
2013-06-01 15:59:02 +0800

    CRUD[create, retrive, update, delete] or IDUS[insert, delete, update, select] functions added

--- AddressBook/AddressBook.py
+++ AddressBook/AddressBook.py
@@ -80,26 +80,98 @@ def dispHelpInfo():
     print('help         -- show help info')
     print('quit/exit    -- exit this program')
     print('showAll      -- show all address info')
-    print('add          -- add a new address')
+    print('query        -- get an item by name')
+    print('add          -- add a new address item')
+    print('remove/rm    -- remove an item from address book')
+    print('update/up    -- update an item in address book')
     return
 
 #display all items in the address book
 def showAll():
-    for item in g_map:
-        addr  = g_map[item]
-        if(not addr):
-            print('No addr for %s!' % item)
-            return
+    for name, addr in g_map.items():
         email = addr.getEmail()
         phone = addr.getPhone()
         memo  = addr.getMemo()
-        print('contact %s at: ' % item)
+        print('contact %s at: ' % name)
         if(email):
             print('    Email: %s' % email)
         if(phone):
             print('    Phone: %s' % phone)
         if(memo):
             print('    Memo : %s' % memo)
+    return
+
+#find an item in address book
+def find(name):
+    if (name in g_map):
+        addr  = g_map[name]
+        email = addr.getEmail()
+        phone = addr.getPhone()
+        memo  = addr.getMemo()
+        print('contact %s at: ' % name)
+        if(email):
+            print('    Email: %s' % email)
+        if(phone):
+            print('    Phone: %s' % phone)
+        if(memo):
+            print('    Memo : %s' % memo)
+    return
+
+#get an item by name
+def query():
+    name = input("name:")
+    if (name in g_map):
+        find(name)
+    else:
+        print('%s not in this address book' % name)
+    return
+
+#add an item to address book
+def add():
+    name = input("name:")
+    if (not name):
+        print('must set name for an item in this address book')
+        return
+    if (name in g_map):
+        print('%s already in this address book' % name)
+        find(name)
+        return
+    email = input("email:")
+    phone = input("phone:")
+    memo  = input("memo:")
+    addr = Address(name, email, phone, memo)
+    g_map[name] = addr
+    writeToFile()
+    return
+
+#remove an item from address book
+def remove():
+    name = input("name:")
+    if (name not in g_map):
+        print('%s not in this address book' % name)
+        return
+    del g_map[name]
+    writeToFile()
+    return
+
+#update an item in address book
+def update():
+    name = input("name:")
+    if (name not in g_map):
+        print('%s not in this address book' % name)
+        return
+    addr  = g_map[name]
+    email = input("email:")
+    if (email):
+        addr.setEmail(email)
+    phone = input("phone:")
+    if (phone):
+        addr.setPhone(phone)
+    memo  = input("memo:")
+    if (memo):
+        addr.setMemo(memo)
+    g_map[name] = addr
+    writeToFile()
     return
 
 #get user's command and process it
@@ -110,6 +182,15 @@ while cmd not in cmd_quit:
         dispHelpInfo()
     if('showAll' == cmd):
         showAll()
+    if('query' == cmd):
+        query()
+    if('add' == cmd):
+        add()
+    if('rm' == cmd or 'remove' == cmd):
+        remove()
+    if('up' == cmd or 'update' == cmd):
+        update()
+        
     #get the nexe command
     cmd = input("CMD:")
 else:
